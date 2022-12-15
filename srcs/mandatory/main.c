491a23b21d772eb5c296fde6c5ac5965d700cede
mohazerro <mohazerro@github.com>
2022-12-15 18:45:58 +0100

    fifth

--- srcs/mandatory/main.c
+++ srcs/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: mgamil <mgamil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 05:09:49 by mgamil            #+#    #+#             */
-/*   Updated: 2022/12/14 23:08:07 by mgamil           ###   ########.fr       */
+/*   Updated: 2022/12/15 18:43:29 by mgamil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,53 +62,85 @@ void	exec(t_args *args, int boolean, int index)
 	int		i;
 
 	i = -1;
+	tab = ft_split(args->cmds[index], ' ');
 	while (boolean && args->env[++i])
 	{
-		tab = ft_split(args->cmds[index], ' ');
 		temp = ft_slash(args->env[i], tab[0]);
 		if (args->env[i + 1] == NULL)
 			checkaccess(tab[0]);
+		// ft_printf("(%d)about to exec\n", index);
 		execve(temp, tab, NULL);
-		ft_freestr(1, 1, temp, tab);
+		// ft_printf("(%d)failed exec\n", index);
+		free(temp);
 	}
 	if (!boolean)
 	{
-		tab = ft_split(args->cmds[index], ' ');
 		checkaccess(tab[0]);
 		execve(tab[0], tab, NULL);
 		perror("slashback");
-		ft_freetab((void **)tab);
 	}
+	ft_freetab((void **)tab);
 }
+
+// void	forking(t_args *args, int index, char **av)
+// {
+// 	(void)av;
+// 	// if (index == 0 || index == args->nbcmds - 1)
+// 	if (index == 0)
+// 	{
+// 		// args->infile = open(av[index], O_RDONLY);
+// 		// ft_printf("(%d)INFILE EEEE%i\n", index, args->infile);
+// 		dup2(args->infile, STDIN_FILENO);
+// 		close(args->infile);
+// 	}
+// 	else if (index == args->nbcmds - 1)
+// 	{
+// 		// args->outfile = open(av[args->nbcmds - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+// 		// ft_printf("(%d)OUTFILE EEE %i\n", index, args->outfile);
+// 		dup2(args->outfile, STDOUT_FILENO);
+// 		close(args->outfile);
+// 	}
+// 	if (index != args->nbcmds - 1)
+// 		dup2(args->fd[1], STDOUT_FILENO);
+// 	if (index != 0)
+// 	{
+// 		dup2(args->prev_pipes, STDIN_FILENO);
+// 		close(args->prev_pipes);
+// 	}
+// 	close(args->fd[0]);
+// }
 
 void	forking(t_args *args, int index)
 {
-	if (index == 0 || index == args->nbcmds - 1)
-		close(args->fd[0]);
-	if (index == 0)
+	if (index == 0) // cat
 	{
+		close(args->fd[0]);
 		dup2(args->infile, STDIN_FILENO);
+		dup2(args->fd[1], STDOUT_FILENO);
 		close(args->infile);
 	}
-	else if (index == args->nbcmds - 1)
-	{
-		dup2(args->outfile, STDOUT_FILENO);
-		close(args->outfile);
-	}
-	if (index != args->nbcmds - 1)
-		dup2(args->fd[1], STDOUT_FILENO);
-	if (index != 0)
+	else if (index == args->nbcmds - 1) // wc -l
 	{
 		dup2(args->prev_pipes, STDIN_FILENO);
+		dup2(args->outfile, STDOUT_FILENO);
+		close(args->outfile);
 		close(args->prev_pipes);
+		close(args->fd[0]);
+	}
+	else
+	{
+		dup2(args->prev_pipes, STDIN_FILENO);
+		dup2(args->fd[1], STDOUT_FILENO);
+		close(args->prev_pipes);
+		close(args->fd[0]);
 	}
 }
-
-void	preforking(t_args *args)
+void	preforking(t_args *args, char **av)
 {
 	int	i;
 
 	i = -1;
+	(void)av;
 	while (++i < args->nbcmds)
 	{
 		pipe(args->fd);
@@ -117,8 +149,7 @@ void	preforking(t_args *args)
 		{
 			forking(args, i);
 			close(args->fd[1]);
-			ft_printf("lol\n");
-			if (args->cmds[i][0] == '/')
+			if (ft_strchr(args->cmds[i], '/'))
 				exec(args, 0, i);
 			else
 				exec(args, 1, i);
@@ -137,6 +168,8 @@ int	main(int ac, char *av[], char *envp[])
 {
 	t_args	*args;
 
+	if (!envp[0])
+		return (1);
 	args = ft_calloc(sizeof(t_args), 1);
 	if (!args)
 		exit(1);
@@ -145,11 +178,11 @@ int	main(int ac, char *av[], char *envp[])
 		return (1);
 	init(args, av, ac);
 	// checkcmd(args);
-	ft_printstruct(args, ac);
-	preforking(args);
+	preforking(args, av);
 	wait_pids(args);
+	// ft_printstruct(args, ac);
 	// while (wait(NULL) > 0)
-		// ;
+	// ;
 	close(args->outfile);
 	close(args->fd[0]);
 	close(args->infile);
